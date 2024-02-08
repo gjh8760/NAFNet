0317be467f51d11de8ca11e1da9773c40255a24b
gjh8760 <gjh8760@snu.ac.kr>
2024-02-08 15:21:22 +0900

    update

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon Sep 11 14:53:54 2023
-__version__ = '1.2.0+50cb149'
+# TIME: Thu Feb  8 15:20:51 2024
+__version__ = '1.2.0+2aac2bf'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
